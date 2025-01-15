2b78403c3339106145515b646d79953f3aaab7c1
Keyner-Mena <keynermena896@gmail.com>
2025-01-15 16:11:05 -0600

    Actualizacion Comentarios

--- comentarios/Comentarios.js
+++ comentarios/Comentarios.js
@@ -1,69 +1,49 @@
-var stars = document.getElementsByClassName("star");// Obtener todas las estrellas
+var stars = document.getElementsByClassName("star"); // Obtener todas las estrellas
 var starSelected = false; // Variable para indicar si se ha seleccionado una estrella
 var Cantidad = 0;
 
 function rate(num) {
-    // Resaltar todas las estrellas anteriores a la seleccionada
-    for (var i = 0; i < num; i++) {
-        stars[i].classList.add("highlight");
-    }
+  // Resaltar todas las estrellas anteriores a la seleccionada
+  for (var i = 0; i < num; i++) {
+    stars[i].classList.add("highlight");
+  }
 
-    // Quitar el resaltado de las estrellas siguientes a la seleccionada
-    for (var i = num; i < stars.length; i++) {
-        stars[i].classList.remove("highlight");
-    }
+  // Quitar el resaltado de las estrellas siguientes a la seleccionada
+  for (var i = num; i < stars.length; i++) {
+    stars[i].classList.remove("highlight");
+  }
 
-    starSelected = true;
-    Cantidad = num;
+  starSelected = true;
+  Cantidad = num;
 }
 
-
 function submitFeedback() {
-    //Guardar comentario
-    const commentText = document.getElementById('commentText').value;
-    //Crear alerta
-    var alerta = document.createElement("div");
-    alerta.setAttribute("role", "alert");
+  //Guardar comentario
+  const commentText = document.getElementById("commentText").value;
+  //Crear alerta
+  var alerta = document.createElement("div");
+  alerta.setAttribute("role", "alert");
 
-
-    if (starSelected && commentText) {
-        alerta.classList.add("alert", "alert-success");
-        alerta.textContent = `Gracias por tu calificacion de ` + Cantidad + ` estrellas y tu comentario.`;
-        document.body.appendChild(alerta);
-        // Hacer scroll hasta el final de la página
-        alerta.scrollIntoView();
-
-        // Esperar 2s
-        setTimeout(function () {
-            location.reload()
-        }, 2000);
-
+  if (starSelected && commentText) {
+    alerta.classList.add("alert", "alert-success");
+    alerta.textContent =
+      `Gracias por tu calificacion de ` +
+      Cantidad +
+      ` estrellas y tu comentario.`;
+  } else {
+    if (starSelected) {
+      alerta.classList.add("alert", "alert-success");
+      alerta.textContent =
+        `Gracias por tu calificación de ` + Cantidad + ` estrellas.`;
     } else {
-
-        if (starSelected) {
-
-            alerta.classList.add("alert", "alert-success");
-            alerta.textContent = `Gracias por tu calificacion de ` + Cantidad + ` estrellas.`;
-            document.body.appendChild(alerta);
-            alerta.scrollIntoView();
-            // Esperar 2s
-            setTimeout(function () {
-                location.reload()
-            }, 2000);
-
-        } else {
-
-            alerta.classList.add("alert", "alert-danger");
-            alerta.textContent = 'Por favor selecciona una calificación.';
-            document.body.appendChild(alerta);
-            alerta.scrollIntoView();
-
-            // Esperar 2s
-            setTimeout(function () {
-                document.body.removeChild(alerta);
-            }, 2000);
-
-        }
-
+      alerta.classList.add("alert", "alert-danger");
+      alerta.textContent = "Por favor selecciona una calificación.";
     }
+  }
+  document.body.appendChild(alerta);
+  alerta.scrollIntoView();
+  // Esperar 2s
+  setTimeout(function () {
+    location.reload();
+  }, 2000);
 }
